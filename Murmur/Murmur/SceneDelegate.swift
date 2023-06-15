fd57243a25d8b67642cd9732787e19ca0b63afee
cleopatra <kelly912718@gmail.com>
2023-06-15 11:53:49 +0800

    [General] Add swiftlint and modify swiftlint.yml.

--- Murmur/Murmur/SceneDelegate.swift
+++ Murmur/Murmur/SceneDelegate.swift
@@ -7,16 +7,19 @@
 
 import UIKit
 
+// swiftlint:disable line_length
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        
+        // swiftlint:disable unused_optional_binding
         guard let _ = (scene as? UIWindowScene) else { return }
+        // swiftlint:enable unused_optional_binding
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -47,6 +50,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
     }
 
-
 }
 
+// swiftlint:enable line_length
