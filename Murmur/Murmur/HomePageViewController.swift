8cae2b6d1515ff2b7c2dcaa442d950bd05814f8c
cleopatra <kelly912718@gmail.com>
2023-06-17 08:44:00 +0800

    [Feature] Import map and locate myself.

--- Murmur/Murmur/HomePageViewController.swift
+++ Murmur/Murmur/HomePageViewController.swift
@@ -7,49 +7,89 @@
 
 import UIKit
 import SnapKit
+import MapKit
+import CoreLocation
 
 class HomePageViewController: UIViewController {
     
-    let mapView: UIView = {
-        let mapView = UIView()
+    // 1.創建 locationManager
+    private let locationManager = CLLocationManager()
+    private var monitoredRegions: Dictionary<String, Date> = [:]
+    
+    private let mapView: MKMapView = {
+        let mapView = MKMapView()
         mapView.translatesAutoresizingMaskIntoConstraints = false
         mapView.backgroundColor = .white
         
         return mapView
     }()
-    
-    let btnStack: UIStackView = {
+    private let btnStack: UIStackView = {
         let btnStack = UIStackView()
         btnStack.axis = .vertical
         return btnStack
     }()
-    let filterButton: UIButton = {
+    private let filterButton: UIButton = {
         let filterButton = UIButton()
         filterButton.backgroundColor = .black
         filterButton.setImage(UIImage(named: "Icons_Filter"), for: .normal)
         return filterButton
     }()
-    let locationMessageButton: UIButton = {
+    private let locationMessageButton: UIButton = {
         let locationMessageButton = UIButton()
         locationMessageButton.backgroundColor = .blue
         locationMessageButton.setImage(UIImage(named: "Icons_People"), for: .normal)
         return locationMessageButton
     }()
-    let locateButton: UIButton = {
+    private let locateButton: UIButton = {
         let locateButton = UIButton()
         locateButton.backgroundColor = .red
         locateButton.setImage(UIImage(named: "Icons_Locate"), for: .normal)
         return locateButton
     }()
-    
+    let alertController: UIAlertController = {
+        let alertController = UIAlertController(title: "建議", message: "請開啟你的定位服務以繼續使用 app", preferredStyle: .alert)
+        // swiftlint:disable line_length
+        //    let alertController = UIAlertController(title: "建議", message: "Location services were previously denied. Please enable location services for this app in Settings.", preferredStyle: .alert)
+        // swiftlint:enable line_length
+        alertController.addAction(UIAlertAction(title: "確定", style: .default))
+        alertController.addAction(UIAlertAction(title: "取消", style: .cancel, handler: nil))
+        // TODO: 加上 handler closure 引導使用者開定位功能
+        return alertController
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = .red
         setMapView()
+        setLocation()
     }
     
-    func setMapView() {
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+//        // 1. 還沒有詢問過用戶以獲得權限
+//        if CLLocationManager.authorizationStatus() == .notDetermined {
+//            locationManager.requestAlwaysAuthorization()
+//        }
+//        // 2. 用戶不同意
+//        else if CLLocationManager.authorizationStatus() == .denied {
+//            present(alertController, animated: true)
+//        }
+//        // 3. 用戶已經同意
+//        else if CLLocationManager.authorizationStatus() == .authorizedAlways {
+//            locationManager.startUpdatingLocation()
+//        }
+        
+        if CLLocationManager.authorizationStatus() == .notDetermined {
+                // 取得定位服務授權
+                locationManager.requestWhenInUseAuthorization()
+                // 開始定位自身位置
+                locationManager.startUpdatingLocation()
+            }
+    }
+    
+    private func setMapView() {
         self.view.addSubview(mapView)
         self.view.addSubview(btnStack)
         [filterButton, locationMessageButton, locateButton].forEach { subview in
@@ -85,6 +125,102 @@ class HomePageViewController: UIViewController {
             make.width.equalTo(48)
             make.height.equalTo(locateButton.snp.width)
         }
+    }
+    
+    private func setupData() {
+        // 1. 檢查系統是否能夠監視 region
+        if CLLocationManager.isMonitoringAvailable(for: CLCircularRegion.self) {
+
+            // 2.準備 region 會用到的相關屬性
+            let title = "未知玩家"
+            let coordinate = CLLocationCoordinate2DMake(25.03889164303853, 121.53317942146191)
+            let regionRadius = 200.0 // 範圍半徑
+
+            // 3. 設置 region 的相關屬性
+            let region = CLCircularRegion(center: CLLocationCoordinate2D(latitude: coordinate.latitude,
+                longitude: coordinate.longitude), radius: regionRadius, identifier: title)
+            locationManager.startMonitoring(for: region)
+
+            // 4. 創建大頭釘(annotation)
+            let restaurantAnnotation = MKPointAnnotation()
+            restaurantAnnotation.coordinate = coordinate
+            restaurantAnnotation.title = "\(title)"
+            mapView.addAnnotation(restaurantAnnotation)
+
+            // 5. 繪製一個圓圈圖形（用於表示 region 的範圍）
+            let circle = MKCircle(center: coordinate, radius: regionRadius)
+            mapView.addOverlay(circle)
+        } else {
+            print("System can't track regions")
+        }
+    }
+    
+    // MARK: - Comples business logic
+    func updateRegionsWithLocation(_ location: CLLocation) {
+
+        let regionMaxVisiting = 10.0
+        var regionsToDelete: [String] = []
+
+        for regionIdentifier in monitoredRegions.keys {
+            if Date().timeIntervalSince(monitoredRegions[regionIdentifier]!) > regionMaxVisiting {
+                print("Thanks for visiting our restaurant")
+
+                regionsToDelete.append(regionIdentifier)
+            }
+        }
+
+        for regionIdentifier in regionsToDelete {
+            monitoredRegions.removeValue(forKey: regionIdentifier)
+        }
+    }
+    
+}
+
+extension HomePageViewController: MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    private func setLocation() {
+        
+        // 2. 配置 locationManager
+        locationManager.delegate = self
+        locationManager.distanceFilter = kCLLocationAccuracyBestForNavigation
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        // 3. 配置 mapView
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .follow
+        
+        // 4. 加入測試數據
+        setupData()
         
     }
+    
+    
+    // MARK: - MKMapViewDelegate
+    // 6. 繪製圓圈
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let circleRenderer = MKCircleRenderer(overlay: overlay)
+        circleRenderer.strokeColor = UIColor.red
+        circleRenderer.lineWidth = 1.0
+        return circleRenderer
+    }
+    
+    // MARK: - CLLocationManagerDelegate
+    // 1. 當用戶進入一個 region
+    private func locationManager(manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        print("enter \(region.identifier)")
+        monitoredRegions[region.identifier] = Date()
+    }
+
+    // 2. 當用戶退出一個 region
+    private func locationManager(manager: CLLocationManager, didExitRegion region: CLRegion) {
+        print("exit \(region.identifier)")
+        monitoredRegions.removeValue(forKey: region.identifier)
+    }
+    
+    // TODO: internal
+    internal func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        updateRegionsWithLocation(locations[0])
+    }
+    
 }
---
+++ Murmur/Murmur/LocationMessageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LocationMessageViewController.swift
+//  Murmur
+//
+//  Created by cleopatra on 2023/6/16.
+//
+
+import UIKit
+
+class LocationMessageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+}
